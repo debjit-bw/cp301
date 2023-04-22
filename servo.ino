34d2ed6b1d35e63bfcc277b6bc20846ff3243dd7
Debjit Bhowal <68442560+debjit-bw@users.noreply.github.com>
2023-04-22 15:53:43 +0530

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -1,22 +1,3 @@
-/*
-  Arduino Starter Kit example
-  Project 5 - Servo Mood Indicator
-
-  This sketch is written to accompany Project 5 in the Arduino Starter Kit
-
-  Parts required:
-  - servo motor
-  - 10 kilohm potentiometer
-  - two 100 uF electrolytic capacitors
-
-  created 13 Sep 2012
-  by Scott Fitzgerald
-
-  https://store.arduino.cc/genuino-starter-kit
-
-  This example code is part of the public domain.
-*/
-
 // include the Servo library
 #include <Servo.h>
 
