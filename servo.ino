7ae16743a5b92611c39184fd30d0a13a92eaad85
Debjit Bhowal <68442560+debjit-bw@users.noreply.github.com>
2023-04-22 14:33:54 +0530

    Create servo.ino

---
+++ servo.ino
@@ -0,0 +1,44 @@
+/*
+  Arduino Starter Kit example
+  Project 5 - Servo Mood Indicator
+
+  This sketch is written to accompany Project 5 in the Arduino Starter Kit
+
+  Parts required:
+  - servo motor
+  - 10 kilohm potentiometer
+  - two 100 uF electrolytic capacitors
+
+  created 13 Sep 2012
+  by Scott Fitzgerald
+
+  https://store.arduino.cc/genuino-starter-kit
+
+  This example code is part of the public domain.
+*/
+
+// include the Servo library
+#include <Servo.h>
+
+Servo myServo;  // create a servo object
+
+int pos = 0;
+
+void setup() {
+  myServo.attach(9);   // attaches the servo on pin 9 to the servo object
+  // Serial.begin(9600);  // open a serial connection to your computer
+}
+
+void loop() {
+  for (pos = 0; pos <= 720; pos += 1) {
+    myServo.write(pos);
+    delay(15);
+  }
+  delay(1000);
+
+  for (pos = 720; pos >= 0; pos -= 1) {
+    myServo.write(pos);
+    delay(15);
+  }
+  delay(1000);
+}
