408efd2a635f8e413064b465eea559f93da1c19d
Debjit Bhowal <68442560+debjit-bw@users.noreply.github.com>
2023-04-22 15:30:35 +0530

    Update dc.ino

--- dc.ino
+++ dc.ino
@@ -1,58 +1,29 @@
-#include <Arduino.h>
+// DC motor 1 control
+#define P1A 8 // define pin 2 as for P1A
+#define P2A 7 // define pin 7 as for P2A
 
-const int motorPinA = 3; // PWM pin for motor speed control
-const int motorPinB = 5; // PWM pin for motor speed control
-const int in1 = 8; // Motor direction control pin
-const int in2 = 9; // Motor direction control pin
-const int in3 = 10; // Motor direction control pin
-const int in4 = 11; // Motor direction control pin
-const int potPin = A0; // Analog pin for potentiometer
-const int buttonPin = 2; // Digital pin for push button
-
+// the setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(motorPinA, OUTPUT);
-  pinMode(motorPinB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  pinMode(potPin, INPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(9600);// setup Serial Monitor to display information
+  pinMode(P1A, OUTPUT);// define pin as OUTPUT for P1A
+  pinMode(P2A, OUTPUT);// define pin as OUTPUT for P2A
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  int potVal = analogRead(potPin); // Read potentiometer value
-  int motorSpeed = map(potVal, 0, 1023, 0, 255); // Map potentiometer value to motor speed (0-255)
+  Serial.println(" Rotating CW");
+  // digitalWrite(EN12 ,HIGH);// Enable 1A and 2A 
+  digitalWrite(P1A,LOW);// send + or HIGH singal to P1A
+  digitalWrite(P2A,HIGH);// send - or LOW singal to P2A       
+  delay(1000);// motor runs for 3 seconds
+  // digitalWrite(EN12 ,LOW);// Disable 1A and 2A
+  
+  digitalWrite(P1A,HIGH);// send + or HIGH singal to P1A
+  digitalWrite(P2A,LOW);// send - or LOW singal to P2A   
 
-  bool buttonState = digitalRead(buttonPin); // Read button state (LOW = pressed, HIGH = not pressed)
+  // delay(1000);// motor stop for 3 seconds 
+  Serial.println(" Motor Stopped"); 
 
-  if (buttonState == LOW) { // If button is pressed, set motor direction to forward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-  } else { // If button is not pressed, set motor direction to reverse
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
-
-  analogWrite(motorPinA, motorSpeed); // Set motor speed
-  analogWrite(motorPinB, motorSpeed); // Set motor speed
+  Serial.println("=========== Loop done"); 
+  delay(1000);
 }
-
-///
-/// more on: https://www.phind.com/search?cache=abe4e0d3-2d8c-4d47-9ee0-5657139f4981
-// Based on your request, I've written an Arduino code snippet to control a Tetrix DC motor using an L293D motor driver. In this example, I assume you want to control the motor speed and direction using a potentiometer and a push button, similar to the example provided in howtomechatronics.com (https://howtomechatronics.com/tutorials/arduino/arduino-dc-motor-control-tutorial-l298n-pwm-h-bridge/).
-
-// In this code, the motor speed is controlled using a potentiometer connected to the analog pin A0. The motor direction is controlled using a push button connected to the digital pin 2. The L293D motor driver is connected to the Arduino using the following pins:
-
-// Motor A: PWM pin 3 (motorPinA), direction control pins 8 (in1) and 9 (in2)
-// Motor B: PWM pin 5 (motorPinB), direction control pins 10 (in3) and 11 (in4)
-// The motor speed is determined by reading the potentiometer value and mapping it to a range of 0-255. The motor direction is determined by the push button state (LOW = forward, HIGH = reverse).
-
-// Keep in mind that this example is for controlling two DC motors. If you want to control a single motor, you can simply remove the lines related to the second motor (motorPinB, in3, and in4) and adjust the code accordingly.
-
-// I hope this helps! Let me know if you have any questions or need further clarification.
-///
