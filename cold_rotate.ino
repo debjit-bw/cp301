ff2f002d9231b1869e42e91609ffae2e57e25281
Debjit Bhowal <68442560+debjit-bw@users.noreply.github.com>
2023-05-07 14:47:07 +0530

    dual motor and fwd drive support added

--- cold_rotate.ino
+++ cold_rotate.ino
@@ -2,8 +2,11 @@
 #include <Adafruit_AMG88xx.h>
 #include <stdarg.h>
 // DC motor 1 control
-#define P1A 8 // define pin 2 as for P1A
-#define P2A 7 // define pin 7 as for P2A
+#define P1A 2 // define pin 2 as for P1A
+#define P2A 4 // define pin 4 as for P2A
+// DC motor 2 control
+#define P1B 7 // define pin 7 as for P1B
+#define P2B 8 // define pin 8 as for P2B
 
 void Serialprintln(const char* input...) {
   va_list args;
@@ -39,12 +42,14 @@ char getDirection(float values[3]) {
 char getDirectionInv(float values[3]) {
   float leftAvg = (values[0] + values[1]) / 2.0; // compute average of left values
   float rightAvg = (values[1] + values[2]) / 2.0; // compute average of right values
-  if (values[2] < 20 || values[2] < (leftAvg - 5.0)) { // right is significantly lower than left and center
+  if (values[1] < 20 || (values[1] < (values[0] - 5.0) && values[1] < (values[2] - 5.0))) {
+    return 'c';
+  } else if (values[2] < 20 || values[2] < (leftAvg - 5.0)) { // right is significantly lower than left and center
     return 'r';
   } else if (values[0] < 20 || values[0] < (rightAvg - 5.0)) { // left is significantly lower than right and center
     return 'l';
   } else { // center is the lowest or values are not significantly different
-    return 'c';
+    return 'n';
   }
 }
 
@@ -52,20 +57,23 @@ char getDirectionInv(float values[3]) {
 void justRotateMotor(char direction) {
   Serial.print("Rotating ");
   if (direction == 'l') { // rotate counterclockwise for 'l'
-    Serial.println("CCW");
+    Serial.println("left");
+    digitalWrite(P1B,HIGH);
+    digitalWrite(P2B,LOW);
+  } else if (direction == 'r') { // rotate clockwise for 'r'
+    Serial.println("right");
     digitalWrite(P1A,HIGH);
     digitalWrite(P2A,LOW);
-  } else if (direction == 'r') { // rotate clockwise for 'r'
-    Serial.println("CW");
-    digitalWrite(P1A,LOW);
+  } else if (direction == 'c') { // drive both motors for a moment for 'c'
+    Serial.println("fwd");
+    digitalWrite(P1A,HIGH);
     digitalWrite(P2A,HIGH);
   } else { // do not rotate for any other input
-    // Serial.println("Invalid direction");
-    // TODO: Drive both motors here
+    Serial.println("none");
     return;
   }
 
-  delay(200); // rotate for 200ms
+  delay(200); // rotate/drive for 200ms
   digitalWrite(P1A,LOW);
   digitalWrite(P2A,LOW);
 }
@@ -75,25 +83,8 @@ Adafruit_AMG88xx amg;
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 float temps[3];
 
-void setup() {
-    Serial.begin(9600);
-
-    bool status;
-    
-    // default settings
-    status = amg.begin();
-    if (!status) {
-        Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
-        while (1);
-    }
-
-    pinMode(P1A, OUTPUT);// define pin as OUTPUT for P1A
-    pinMode(P2A, OUTPUT);// define pin as OUTPUT for P2A
-
-    delay(100); // let sensor boot up
-}
-
-void loop() {
+float* readTemps() {
+  static float temps[3];
   // Read temperature values from sensor
   amg.readPixels(pixels);
   temps[0] = temps[1] = temps[2] = 0;
@@ -107,6 +98,34 @@ void loop() {
       if (pixels[i] > temps[1]) temps[1] = pixels[i];
     }
   }
+
+  return temps;
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    bool status;
+    
+    // default settings
+    status = amg.begin();
+    if (!status) {
+        Serial.println("Wrong wiring");
+        while (1);
+    }
+
+    pinMode(P1A, OUTPUT);// define pin as OUTPUT for P1A
+    pinMode(P2A, OUTPUT);// define pin as OUTPUT for P2A
+
+    pinMode(P1B, OUTPUT);// define pin as OUTPUT for P1B
+    pinMode(P2B, OUTPUT);// define pin as OUTPUT for P2B
+
+    delay(100); // let sensor boot up
+}
+
+void loop() {
+  // Read temperature values from sensor
+  float* temps = readTemps();
 
   Serialprintln("[%f\t%f\t%f]\n", temps[0], temps[1], temps[2]);
   // Determine direction to rotate motor
